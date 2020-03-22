fd7da4c9bf132a7494ed5be41ddfcf1471490d34
shirokuma-exp <herb9mond@gmail.com>
2020-03-22 23:12:14 +0900

    edit_credit_setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
 
-  resources :cards,only:[:index,:new,:show]
+  resources :cards, only: [:new, :show, :index, :edit] do
+    collection do
+      post 'pay', to: 'cards#pay'
+      post 'delete', to: 'cards#delete'
+      post 'buy', to: 'cards#buy'
+    end
+  end
   resources :items, only: [:new, :create, :edit, :update, :show, :destroy]
   resources :users, only: [:edit, :update] do
     resources :addresses, only: [:new, :create]
