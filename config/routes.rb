86ae455551df0907233bd5a4450848601529b7c8
shirokuma-exp <herb9mond@gmail.com>
2020-03-22 10:37:32 +0900

    create-credit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  resources :items, only: [:new, :create, :edit, :update, :show, :destroy]
+  resources :users, only: [:edit, :update] do
+    resources :addresses, only: [:new, :create]
+  end
+  resources :cards,only:[:index,:new,:show]do
+    collection do
+      #payjpでトークン化を行う
+      post 'pay', to: 'cards#pay'
+      #カード削除
+      post 'delete', to: 'cards#delete'
+      #カード情報入力
+      post 'show', to: 'cards#show'
+    end
+  end
 end
