5fc40538799b0e6b639c6dad16cd7d6dabb02fee
shirokuma-exp <herb9mond@gmail.com>
2020-03-25 19:51:16 +0900

    edit-category.js

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,9 +1,8 @@
-
 document.addEventListener("turbolinks:load", function(){
   $(function(){
     // カテゴリーセレクトボックスのオプションを作成
     function appendOption(category){
-      var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+      var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
       return html;
     }
     // 子カテゴリーの表示作成
@@ -11,7 +10,7 @@ document.addEventListener("turbolinks:load", function(){
       var childSelectHtml = '';
       childSelectHtml = `<div class='listing-select-wrapper__added' id= 'children_wrapper'>
                           <div class='listing-select-wrapper__box'>
-                            <select class="listing-select-wrapper__box--select" id="child_category" name="category_id">
+                            <select class="listing-select-wrapper__box--select select-test" id="child_category" name="category_id">
                               <option value="---" data-category="---">---</option>
                               ${insertHTML}
                             <select>
@@ -25,7 +24,7 @@ document.addEventListener("turbolinks:load", function(){
       var grandchildSelectHtml = '';
       grandchildSelectHtml = `<div class='listing-select-wrapper__added' id= 'grandchildren_wrapper'>
                                 <div class='listing-select-wrapper__box'>
-                                  <select class="listing-select-wrapper__box--select" id="grandchild_category" name="category_id">
+                                  <select class="listing-select-wrapper__box--select select-test" id="grandchild_category" name="item[category_id]">
                                     <option value="---" data-category="---">---</option>
                                     ${insertHTML}
                                   </select>
@@ -39,13 +38,14 @@ document.addEventListener("turbolinks:load", function(){
       var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
       if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
         $.ajax({
-          url: 'get_category_children',
+          url: '/items/get_category_children',
           type: 'GET',
           data: { parent_name: parentCategory },
           dataType: 'json'
         })
         .done(function(children){
-          $('#children_wrapper').remove(); //親が変更された時、子以下を削除する
+          $('#children_wrapper').remove(); //親が変更された時、子以下を削除するする
+          $('.listing-select-wrapper__box--arrow-down').remove();
           $('#grandchildren_wrapper').remove();
           $('#size_wrapper').remove();
           $('#brand_wrapper').remove();
@@ -59,7 +59,8 @@ document.addEventListener("turbolinks:load", function(){
           alert('カテゴリー取得に失敗しました');
         })
       }else{
-        $('#children_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除する
+        $('#children_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
+        $('.listing-select-wrapper__box--arrow-down').remove();
         $('#grandchildren_wrapper').remove();
         $('#size_wrapper').remove();
         $('#brand_wrapper').remove();
@@ -70,14 +71,14 @@ document.addEventListener("turbolinks:load", function(){
       var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
       if (childId != "---"){ //子カテゴリーが初期値でないことを確認
         $.ajax({
-          url: 'get_category_grandchildren',
+          url: '/items/get_category_grandchildren',
           type: 'GET',
           data: { child_id: childId },
           dataType: 'json'
         })
         .done(function(grandchildren){
           if (grandchildren.length != 0) {
-            $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除する
+            $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除するする
             $('#size_wrapper').remove();
             $('#brand_wrapper').remove();
             var insertHTML = '';
