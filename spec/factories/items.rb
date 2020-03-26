123816e2c8e53701fc1bcdd9f5a16502eb67559d
golazooo23 <kengoteshima0823@gmail.com>
2020-03-26 16:58:58 +0900

    Create items.rb

---
+++ spec/factories/items.rb
@@ -0,0 +1,20 @@
+FactoryBot.define do
+
+  factory :item do
+    name                {"hoge"}
+    description         {"hoge"}
+    condition_id        {"1"}
+    size                {"L"}
+    delivery_charge_id  {"1"}
+    delivery_way_id     {"1"}
+    shipping_period_id  {"1"}
+    region_id           {"1"}
+    price               {"1000"}
+    like                {"hoge"}
+    buyer_id            {"1"}
+    category_id         {"1"}
+    brand_name          {"hoge"}
+    user
+  end
+
+end
