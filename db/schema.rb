457bee3dbb93e38e673ae278592a14a610c3cb11
golazooo23 <kengoteshima0823@gmail.com>
2020-03-26 13:21:45 +0900

    brand_id→brand_name
    brandテーブルの作成を取りやめ、itemsテーブルにブランド名を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_032147) do
+ActiveRecord::Schema.define(version: 2020_03_26_041709) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination", null: false
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 2020_03_26_032147) do
     t.bigint "user_id"
     t.integer "buyer_id"
     t.integer "category_id"
-    t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "brand_name"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
