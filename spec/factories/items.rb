5e08e2616ba95c7b6b6e7f1ab213e3436e0e53b2
golazooo23 <kengoteshima0823@gmail.com>
2020-03-27 18:37:35 +0900

    3/27 時点
    editとcreateは未完成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -18,4 +18,26 @@ FactoryBot.define do
     user
   end
 
+  factory :item_with_photo, class: Item do
+    name                {"hoge"}
+    description         {"hoge"}
+    condition_id        {"1"}
+    size                {"L"}
+    delivery_charge_id  {"1"}
+    delivery_way_id     {"1"}
+    shipping_period_id  {"1"}
+    region_id           {"1"}
+    price               {"1000"}
+    like                {"hoge"}
+    buyer_id            {"1"}
+    status              {"1"}
+    category_id         {"1"}
+    brand_name          {"hoge"}
+    user
+
+    after( :create ) do |item|
+      create :image, item: item
+    end
+  end  
+
 end
